39c0a06690577a120897282f3ed0a774b0d6e1b3
Mao Kurihara <mao.kurihara@gmail.com>
2022-07-04 23:11:06 +0900

    station13

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,9 @@
-function changeBackgroundColor(e) {
+function changeBackgroundColor() {
   // ここに背景色を変える処理を書く。
+  if (check.checked) {
+    document.getElementById("text").style.backgroundColor = "red";
+  } else {
+    document.getElementById("text").style.backgroundColor = "";
+  }
 
 }
